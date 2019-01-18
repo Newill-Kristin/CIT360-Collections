28eee38270f258372872f31c716a1f420b0a9f66
Aingealfire <aingealfire@gmail.com>
2019-01-18 13:41:18 -0500

    Initial commit

---
+++ src/com/github/newill/Main.java
@@ -0,0 +1,21 @@
+package com.github.newill;
+
+
+/**
+ * Created by Kristin Newill (aingealfire) on 1/16/2019.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Create Menu
+        MenuView menuView = new MenuView();
+
+        //Display Menu
+        menuView.displayMenu();
+
+    }
+
+
+}
---
+++ src/com/github/newill/Map.java
@@ -0,0 +1,10 @@
+package com.github.newill;
+
+/**
+ * Created by Kristin Newill (aingealfire) on 1/16/2019.
+ */
+public class Map {
+
+
+
+}
